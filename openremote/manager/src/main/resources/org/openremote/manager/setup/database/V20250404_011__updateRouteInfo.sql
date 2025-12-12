0853b908e5235ea219f467c994817f2abe10a144
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2025-12-12 14:36:54 +0700

    delete code

--- openremote/manager/src/main/resources/org/openremote/manager/setup/database/V20250404_011__updateRouteInfo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE route_info
-ALTER COLUMN ward_id TYPE int4;
--- openremote/manager/src/main/resources/org/openremote/manager/setup/database/V20250701_01__updateNotification.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX idx_notification_target_id_sent_on ON openremote.notification(target_id, sent_on DESC);
-ALTER TABLE notification ADD column IF NOT exists is_read BOOLEAN NOT NULL DEFAULT FALSE;
